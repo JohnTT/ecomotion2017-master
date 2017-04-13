1711781bf0edf0d528720ef9a584318211a1cd18
JohnChen <jmc_tt@live.com>
2017-04-13 16:44:17 -0500

    Added AllCell BMS CAN ID + structs

--- master_datalogger/Inc/master.h
+++ master_datalogger/Inc/master.h
@@ -8,6 +8,86 @@
 #ifndef MASTER_H_
 #define MASTER_H_
 
+// AllCell BMS CAN Bus
+
+/*
+ * Use this function to parse CAN byte stream into structures.
+ * memcpy(&parsedData, data, sizeof(data));
+ */
+
+typedef enum {
+	AllCell_Bat_State_ID = 0x0CFF2020,
+	AllCell_Bat_Info_ID = 0x0CFF2120,
+	AllCell_Bat_Current_ID = 0x18FF8020,
+	AllCell_Bat_Voltage_ID =  0x18FF8120,
+	AllCell_Bat_Temperature_ID = 0x18FF8220,
+	AllCell_Bat_Status_ID =  0x18FF8320,
+	AllCell_Bat_PwAvailable_ID = 0x18FF8420,
+	AllCell_Bat_RTC_ID = 0x18FFD020
+} AllCell_BMS_CAN_ID;
+
+typedef struct {
+	uint8_t State;
+	uint16_t Fault_Code;
+	uint16_t Timer;
+	uint16_t Reset;
+	uint8_t Balance;
+} AllCell_Bat_State;
+
+typedef struct {
+	uint16_t Current;
+	uint16_t Voltage;
+	uint8_t Temp;
+	uint16_t Impedance;
+} AllCell_Bat_Info;
+
+typedef struct {
+	uint16_t Current;
+	uint16_t Charge_Limit;
+	uint16_t Discharge_Limit;
+} AllCell_Bat_Current;
+
+typedef struct {
+	uint16_t Voltage;
+	uint16_t Min_Cell_Voltage;
+	uint8_t Nb_Min_Voltage;
+	uint16_t Max_Cell_Voltage;
+	uint8_t Nb_Max_Voltage;
+} AllCell_Bat_Voltage;
+
+typedef struct {
+	uint8_t Temp_BMS;
+	uint8_t Avg_Cell_Temp;
+	uint8_t Min_Cell_Temp;
+	uint8_t Nb_Min_Temp;
+	uint8_t Max_Cell_Temp;
+	uint8_t Nb_Max_Temp;
+	// uint8_t Temp_Ambient; // Unused for this BMS version
+} AllCell_Bat_Temperature;
+
+typedef struct {
+	uint8_t SOC;
+	uint16_t Capacity;
+	// uint8_t SOH; // Unused for this BMS version
+} AllCell_Bat_Status;
+
+typedef struct {
+	uint32_t PwAvailable_Charge;
+	uint32_t PwAvailable_Disharge;
+} AllCell_Bat_PwAvailable;
+
+typedef struct {
+	uint8_t Year;
+	uint8_t Month;
+	uint8_t Day;
+	uint8_t Hour;
+	uint8_t Minute;
+	uint8_t Second;
+} AllCell_Bat_RTC;
+
+
+// **************************
+
 typedef enum {
 	CAN_PACKET_SET_DUTY = 0,
 	CAN_PACKET_SET_CURRENT,
